d9fd1461392874467ef76031f30109c614de5b64
chrislewis <clewis1@companieshouse.gov.uk>
2024-01-24 14:21:37 +0000

    IDVA3-492

--- src/main/java/uk/gov/companieshouse/pscverificationapi/controller/PscVerificationController.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/controller/PscVerificationController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import uk.gov.companieshouse.api.model.pscverification.PscVerificationApi;
 import uk.gov.companieshouse.api.model.pscverification.PscVerificationData;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
 import uk.gov.companieshouse.pscverificationapi.exception.NotImplementedException;
+import uk.gov.companieshouse.pscverificationapi.model.entity.PscVerification;
 
 public interface PscVerificationController {
     default ResponseEntity<PscVerificationApi> createPscVerification(
@@ -19,6 +21,21 @@ public interface PscVerificationController {
         @RequestAttribute(required = false, name = "transaction") final Transaction transaction,
         @RequestBody @Valid @NotNull final PscVerificationData data, final BindingResult result,
         final HttpServletRequest request) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Retrieve PSC verification submission.
+     *
+     * @param transId        the Transaction ID
+     * @param filingResourceId the PSC Filing ID
+     * @throws NotImplementedException implementing classes must perform work
+     */
+    @GetMapping
+    default ResponseEntity<PscVerification> getPscVerification(
+            @PathVariable("transactionId") final String transId,
+            @PathVariable("filingResourceId") final String filingResourceId,
+            final HttpServletRequest request) {
         throw new NotImplementedException();
     }
 }
