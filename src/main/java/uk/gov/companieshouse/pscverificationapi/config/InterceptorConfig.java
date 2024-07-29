c2721500501f98a4a57333f5c7f8834ae1015704
chrislewis <clewis1@companieshouse.gov.uk>
2024-07-29 11:43:02 +0100

    IDVA3-541
    Add logging interceptor.

--- src/main/java/uk/gov/companieshouse/pscverificationapi/config/InterceptorConfig.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/config/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.pscverificationapi.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +8,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import uk.gov.companieshouse.api.interceptor.TransactionInterceptor;
+import uk.gov.companieshouse.pscverificationapi.interceptor.RequestLoggingInterceptor;
 
 /**
  * Configuration class for interceptor logging.
@@ -14,10 +16,14 @@ import uk.gov.companieshouse.api.interceptor.TransactionInterceptor;
 @Configuration
 @ComponentScan("uk.gov.companieshouse.api")
 public class InterceptorConfig implements WebMvcConfigurer {
-    public static final String COMMON_INTERCEPTOR_PATH =
-            "/transactions/{transaction_id}/persons-with-significant-control-verification";
-    public static final String COMMON_INTERCEPTOR_RESOURCE_PATH =
-            COMMON_INTERCEPTOR_PATH + "/{filing_resource_id}";
+
+    private RequestLoggingInterceptor requestLoggingInterceptor;
+
+    @Autowired
+    public void setRequestLoggingInterceptor(final RequestLoggingInterceptor requestLoggingInterceptor) {
+        this.requestLoggingInterceptor = requestLoggingInterceptor;
+    }
+
     /**
      * Set up the interceptors to run against endpoints when the endpoints are called
      * Interceptors are executed in order of configuration
@@ -27,13 +33,18 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(@NonNull final InterceptorRegistry registry) {
         addTransactionInterceptor(registry);
+        addLoggingInterceptor(registry);
     }
 
     private void addTransactionInterceptor(final InterceptorRegistry registry) {
         registry.addInterceptor(transactionInterceptor())
-                .order(1);
+            .order(1);
     }
 
+    private void addLoggingInterceptor(final InterceptorRegistry registry) {
+        registry.addInterceptor(requestLoggingInterceptor)
+            .order(2);
+    }
 
     @Bean("chsTransactionInterceptor")
     public TransactionInterceptor transactionInterceptor() {
