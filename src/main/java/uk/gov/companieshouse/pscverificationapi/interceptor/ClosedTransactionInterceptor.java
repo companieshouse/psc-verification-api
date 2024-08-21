805d1eb38a3f1f2b39cefd8512ef24394d04ea8d
chrislewis <clewis1@companieshouse.gov.uk>
2024-08-21 10:05:57 +0100

    Adding ClosedTransactionInterceptor class and unit tests

---
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/interceptor/ClosedTransactionInterceptor.java
@@ -0,0 +1,35 @@
+package uk.gov.companieshouse.pscverificationapi.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.servlet.HandlerInterceptor;
+import uk.gov.companieshouse.api.AttributeName;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.model.transaction.TransactionStatus;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
+
+public class ClosedTransactionInterceptor implements HandlerInterceptor {
+    private final Logger LOGGER;
+
+    public ClosedTransactionInterceptor(String loggingNamespace) {
+        this.LOGGER = LoggerFactory.getLogger(loggingNamespace);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        Transaction transaction = (Transaction) request.getAttribute(AttributeName.TRANSACTION.getValue());
+        if (transaction != null && TransactionStatus.CLOSED.getStatus().equalsIgnoreCase(transaction.getStatus().getStatus())) {
+            return true;
+        } else {
+            Map<String, Object> debugMap = new HashMap<>();
+            debugMap.put("request_method", request.getMethod());
+            this.LOGGER.errorRequest(request, "ClosedTransactionInterceptor error: " +
+                "transaction is not closed. The filings cannot be generated.", debugMap);
+            response.setStatus(403);
+            return false;
+        }
+    }
+}
