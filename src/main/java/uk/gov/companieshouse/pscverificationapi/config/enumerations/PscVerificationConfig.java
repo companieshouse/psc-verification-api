ea3202e36310a089755f185b12d1b883b81f228c
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-08-23 10:52:13 +0100

    IDVA3-1450 wip

--- src/main/java/uk/gov/companieshouse/pscverificationapi/config/enumerations/PscVerificationConfig.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/config/enumerations/PscVerificationConfig.java
@@ -6,12 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-//import uk.gov.companieshouse.pscverificationapi.enumerations.YamlPropertySourceFactory;
+import uk.gov.companieshouse.pscverificationapi.enumerations.YamlPropertySourceFactory;
 
-//TODO - create a properties file for the validation messages in a new submodule api-enumerations/psc_verification.yml
-// e.g. @PropertySource(value = "classpath:api-enumerations/psc_verification.yml", factory = YamlPropertySourceFactory.class)
 @Configuration
-@PropertySource(value = "classpath:messages.properties")
+@PropertySource(value = "classpath:api-enumerations/psc_verification.yml", factory = YamlPropertySourceFactory.class)
 public class PscVerificationConfig {
 
     @Bean("validation")
