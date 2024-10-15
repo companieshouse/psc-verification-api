a5d30b264a718cf08da26d127b2720d5e853884f
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-10-15 17:52:39 +0100

    Add method and unit tests to get UVID data from PSC

--- src/main/java/uk/gov/companieshouse/pscverificationapi/service/impl/PscLookupServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/service/impl/PscLookupServiceImpl.java
@@ -2,10 +2,15 @@ package uk.gov.companieshouse.pscverificationapi.service.impl;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.identityverification.model.UvidMatch;
 import uk.gov.companieshouse.api.model.psc.PscApi;
 import uk.gov.companieshouse.api.model.pscverification.PscVerificationData;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
@@ -15,6 +20,7 @@ import uk.gov.companieshouse.pscverificationapi.enumerations.PscType;
 import uk.gov.companieshouse.pscverificationapi.exception.FilingResourceNotFoundException;
 import uk.gov.companieshouse.pscverificationapi.exception.PscLookupServiceException;
 import uk.gov.companieshouse.pscverificationapi.service.PscLookupService;
+import uk.gov.companieshouse.pscverificationapi.utils.LogHelper;
 
 @Service
 public class PscLookupServiceImpl implements PscLookupService {
@@ -28,11 +34,22 @@ public class PscLookupServiceImpl implements PscLookupService {
         this.logger = logger;
     }
 
+    /**
+     * Retrieve a PSC by PscVerificationData.
+     *
+     * @param transaction       the Transaction
+     * @param data              the PSC verification data
+     * @param pscType           the PSC Type
+     * @param ericPassThroughHeader includes authorisation for transaction fetch
+     * @return the PSC details, if found
+     * @throws PscLookupServiceException if the PSC was not found or an error occurred
+     */
     @Override
     public PscApi getPsc(final Transaction transaction, final PscVerificationData data,
                                          final PscType pscType, final String ericPassThroughHeader)
         throws PscLookupServiceException {
 
+        final var logMap = LogHelper.createLogMap(transaction.getId());
         String pscAppointmentId = data.pscAppointmentId();
 
         try {
@@ -51,6 +68,7 @@ public class PscLookupServiceImpl implements PscLookupService {
 
         } catch (final ApiErrorResponseException e) {
             if (e.getStatusCode() == HttpStatus.NOT_FOUND.value()) {
+                logger.errorContext(transaction.getId(), UNEXPECTED_STATUS_CODE, e, logMap);
                 throw new FilingResourceNotFoundException(
                     MessageFormat.format("PSC Details not found for {0}: {1} {2}", pscAppointmentId,
                         e.getStatusCode(), e.getStatusMessage()), e);
@@ -60,9 +78,50 @@ public class PscLookupServiceImpl implements PscLookupService {
                     e.getStatusCode(), e.getStatusMessage()), e);
 
         } catch (URIValidationException | IOException e) {
+            logger.errorContext(transaction.getId(), UNEXPECTED_STATUS_CODE, e, logMap);
             throw new PscLookupServiceException(
                 MessageFormat.format("Error Retrieving PSC details for {0}: {1}", pscAppointmentId,
                     e.getMessage()), e);
         }
+    }
+
+    /**
+     * Retrieve a UvidMatch by PscVerificationData.
+     *
+     * @param transaction       the Transaction
+     * @param data              the PSC verification data
+     * @param pscType           the PSC Type
+     * @param ericPassThroughHeader includes authorisation for transaction fetch
+     * @return the UvidMatch, if found
+     * @throws PscLookupServiceException if the PSC was not found or an error occurred
+     */
+    @Override
+    public UvidMatch getUvidMatchFromPscData(final Transaction transaction,
+        final PscVerificationData data, final PscType pscType, final String ericPassThroughHeader)
+        throws PscLookupServiceException {
+
+        PscApi pscData = getPsc(transaction, data, pscType, ericPassThroughHeader);
+        UvidMatch uvidMatch;
+        uvidMatch = getUvidMatch(pscData);
+
+        return uvidMatch;
+    }
+
+    //TODO Update UvidMatch with the full DOB when available from the PscDetails
+    private UvidMatch getUvidMatch(PscApi pscData) {
+        UvidMatch uvidMatch = new UvidMatch();
+
+        List<String> forenames = new ArrayList<>();
+        Optional<String> forename = Optional.ofNullable(pscData.getNameElements().getForename());
+        Optional<String> otherForenames = Optional.ofNullable(pscData.getNameElements().getOtherForenames());
+        Optional<String> surname = Optional.ofNullable(pscData.getNameElements().getSurname());
+
+        forename.ifPresent(forenames::add);
+        otherForenames.ifPresent(names -> forenames.addAll(Arrays.asList(names.split("\\s+"))));
+
+        uvidMatch.setForenames(forenames);
+        uvidMatch.setSurname(surname.orElse(""));
+
+        return uvidMatch;
     }
 }
---
+++ src/test/java/uk/gov/companieshouse/pscverificationapi/service/impl/PscLookupServiceImplTest.java
@@ -0,0 +1,304 @@
+package uk.gov.companieshouse.pscverificationapi.service.impl;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.when;
+import static uk.gov.companieshouse.pscverificationapi.enumerations.PscType.INDIVIDUAL;
+
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpStatusCodes;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.api.error.ApiErrorResponseException;
+import uk.gov.companieshouse.api.handler.exception.URIValidationException;
+import uk.gov.companieshouse.api.handler.psc.PscsResourceHandler;
+import uk.gov.companieshouse.api.handler.psc.request.PscIndividualGet;
+import uk.gov.companieshouse.api.identityverification.model.UvidMatch;
+import uk.gov.companieshouse.api.model.ApiResponse;
+import uk.gov.companieshouse.api.model.psc.NameElementsApi;
+import uk.gov.companieshouse.api.model.psc.PscApi;
+import uk.gov.companieshouse.api.model.pscverification.PscVerificationData;
+import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.sdk.ApiClientService;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.pscverificationapi.enumerations.PscType;
+import uk.gov.companieshouse.pscverificationapi.exception.FilingResourceNotFoundException;
+import uk.gov.companieshouse.pscverificationapi.exception.PscLookupServiceException;
+import uk.gov.companieshouse.pscverificationapi.service.PscLookupService;
+
+@ExtendWith(MockitoExtension.class)
+class PscLookupServiceImplTest extends TestBaseService {
+
+    private static final String PSC_ID = "654321";
+    private static final PscVerificationData PSC_VERIFICATION_DATA =
+        PscVerificationData.newBuilder().pscAppointmentId(PSC_ID).companyNumber(COMPANY_NUMBER)
+            .build();
+
+    @Mock
+    private ApiClientService apiClientService;
+    @Mock
+    private ApiClient apiClient;
+    @Mock
+    private ApiResponse<PscApi> apiResponse;
+    @Mock
+    private PscIndividualGet pscIndividualGet;
+    @Mock
+    private PscsResourceHandler pscsResourceHandler;
+    @Mock
+    private Transaction transaction;
+    @Mock
+    private Logger logger;
+    private PscLookupService testService;
+
+    @BeforeAll
+    public static void setUpForClass() {
+        PscType[] newEnumValues = addNewEnumValue();
+        myMockedEnum = mockStatic(PscType.class);
+        myMockedEnum.when(PscType::values).thenReturn(newEnumValues);
+        mockedValue = newEnumValues[newEnumValues.length - 1];
+        when(mockedValue.name()).thenReturn("UNKNOWN");
+    }
+
+    @AfterAll
+    public static void tearDownForClass() {
+        myMockedEnum.close();
+    }
+
+    @BeforeEach
+    void setUp() {
+        testService = new PscLookupServiceImpl(apiClientService, logger);
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPscIndividualWhenFound() throws IOException, URIValidationException {
+        when(apiResponse.getData()).thenReturn(new PscApi());
+        when(pscIndividualGet.execute()).thenReturn(apiResponse);
+        when(pscsResourceHandler.getIndividual("/company/"
+            + COMPANY_NUMBER
+            + "/persons-with-significant-control/"
+            + INDIVIDUAL.getValue()
+            + "/"
+            + PSC_ID)).thenReturn(pscIndividualGet);
+        when(apiClient.pscs()).thenReturn(pscsResourceHandler);
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenReturn(apiClient);
+
+        var pscApi =
+            testService.getPsc(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL, PASSTHROUGH_HEADER);
+
+        assertThat(pscApi, samePropertyValuesAs(new PscApi()));
+
+    }
+
+    @Test
+    void getPscWhenIoException() throws IOException {
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenThrow(
+            new IOException("get test case"));
+
+        final var thrown = assertThrows(PscLookupServiceException.class,
+            () -> testService.getPsc(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER));
+        assertThat(thrown.getMessage(),
+            is("Error Retrieving PSC details for " + PSC_ID + ": get test case"));
+    }
+
+    @Test
+    void getPscWhenNotFound() throws IOException {
+        final var exception = new ApiErrorResponseException(
+            new HttpResponseException.Builder(HttpStatusCodes.STATUS_CODE_NOT_FOUND, "test case",
+                new HttpHeaders()));
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenThrow(exception);
+
+        final var thrown = assertThrows(FilingResourceNotFoundException.class,
+            () -> testService.getPsc(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER));
+
+        assertThat(thrown.getMessage(),
+            is("PSC Details not found for " + PSC_ID + ": 404 test case"));
+    }
+
+    @Test
+    void getPscWhenErrorRetrieving() throws IOException {
+        final var exception = new ApiErrorResponseException(
+            new HttpResponseException.Builder(HttpStatusCodes.STATUS_CODE_FORBIDDEN, "test case",
+                new HttpHeaders()));
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenThrow(exception);
+
+        final var thrown = assertThrows(PscLookupServiceException.class,
+            () -> testService.getPsc(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER));
+
+        assertThat(thrown.getMessage(),
+            is("Error Retrieving PSC details for " + PSC_ID + ": 403 test case"));
+    }
+
+    //TODO - Add if it is agreed to implement RO verifications
+    @Disabled
+    @Test
+    void getPscWhenTypeNotRecognised() {
+
+        final var thrown = assertThrows(UnsupportedOperationException.class,
+            () -> testService.getPsc(transaction, PSC_VERIFICATION_DATA, mockedValue,
+                PASSTHROUGH_HEADER));
+
+        assertThat(thrown.getMessage(), is("PSC type UNKNOWN not supported for PSC ID " + PSC_ID));
+    }
+
+    @Test
+    void getUvidMatchFromPscDataWhenFound() throws IOException, URIValidationException {
+
+        UvidMatch uvidMatch = new UvidMatch();
+        List<String> forenames =
+            new ArrayList<>(Arrays.asList("Forename1", "Forename2", "Forename3"));
+        uvidMatch.setForenames(forenames);
+        uvidMatch.setSurname("Surname");
+
+        PscApi pscData = new PscApi();
+        NameElementsApi nameElements = new NameElementsApi();
+        nameElements.setForename("Forename1");
+        nameElements.setOtherForenames("Forename2 Forename3");
+        nameElements.setSurname("Surname");
+        pscData.setNameElements(nameElements);
+
+        when(apiResponse.getData()).thenReturn(pscData);
+        when(pscIndividualGet.execute()).thenReturn(apiResponse);
+        when(pscsResourceHandler.getIndividual("/company/"
+            + COMPANY_NUMBER
+            + "/persons-with-significant-control/"
+            + INDIVIDUAL.getValue()
+            + "/"
+            + PSC_ID)).thenReturn(pscIndividualGet);
+        when(apiClient.pscs()).thenReturn(pscsResourceHandler);
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenReturn(apiClient);
+
+        var uvidMatchTest =
+            testService.getUvidMatchFromPscData(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER);
+
+        assertThat(uvidMatchTest, samePropertyValuesAs(uvidMatch));
+
+    }
+
+    @Test
+    void getUvidMatchFromPscDataWhenForenamesBlank() throws IOException, URIValidationException {
+
+        UvidMatch uvidMatch = new UvidMatch();
+        List<String> forenames = new ArrayList<>(List.of());
+        uvidMatch.setForenames(forenames);
+        uvidMatch.setSurname("Surname");
+
+        PscApi pscData = new PscApi();
+        NameElementsApi nameElements = new NameElementsApi();
+        nameElements.setSurname("Surname");
+        pscData.setNameElements(nameElements);
+
+        when(apiResponse.getData()).thenReturn(pscData);
+        when(pscIndividualGet.execute()).thenReturn(apiResponse);
+        when(pscsResourceHandler.getIndividual("/company/"
+            + COMPANY_NUMBER
+            + "/persons-with-significant-control/"
+            + INDIVIDUAL.getValue()
+            + "/"
+            + PSC_ID)).thenReturn(pscIndividualGet);
+        when(apiClient.pscs()).thenReturn(pscsResourceHandler);
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenReturn(apiClient);
+
+        var uvidMatchTest =
+            testService.getUvidMatchFromPscData(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER);
+
+        assertThat(uvidMatchTest, samePropertyValuesAs(uvidMatch));
+
+    }
+
+    @Test
+    void getUvidMatchWhenSurnameBlank() throws IOException, URIValidationException {
+
+        UvidMatch uvidMatch = new UvidMatch();
+        List<String> forenames =
+            new ArrayList<>(Arrays.asList("Forename1", "Forename2", "Forename3"));
+        uvidMatch.setForenames(forenames);
+        uvidMatch.setSurname("");
+
+        PscApi pscData = new PscApi();
+        NameElementsApi nameElements = new NameElementsApi();
+        nameElements.setForename("Forename1");
+        nameElements.setOtherForenames("Forename2 Forename3");
+        pscData.setNameElements(nameElements);
+
+        when(apiResponse.getData()).thenReturn(pscData);
+        when(pscIndividualGet.execute()).thenReturn(apiResponse);
+        when(pscsResourceHandler.getIndividual("/company/"
+            + COMPANY_NUMBER
+            + "/persons-with-significant-control/"
+            + INDIVIDUAL.getValue()
+            + "/"
+            + PSC_ID)).thenReturn(pscIndividualGet);
+        when(apiClient.pscs()).thenReturn(pscsResourceHandler);
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenReturn(apiClient);
+
+        var uvidMatchTest =
+            testService.getUvidMatchFromPscData(transaction, PSC_VERIFICATION_DATA, INDIVIDUAL,
+                PASSTHROUGH_HEADER);
+
+        assertThat(uvidMatchTest, samePropertyValuesAs(uvidMatch));
+
+    }
+
+    @Test
+    void getUvidMatchFromPscDataWhenNotFound() throws IOException {
+
+        final var exception = new ApiErrorResponseException(
+            new HttpResponseException.Builder(HttpStatusCodes.STATUS_CODE_NOT_FOUND, "test case",
+                new HttpHeaders()));
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenThrow(exception);
+
+        final var thrown = assertThrows(FilingResourceNotFoundException.class,
+            () -> testService.getUvidMatchFromPscData(transaction, PSC_VERIFICATION_DATA,
+                INDIVIDUAL, PASSTHROUGH_HEADER));
+
+        assertThat(thrown.getMessage(),
+            is("PSC Details not found for " + PSC_ID + ": 404 test case"));
+
+    }
+
+    @Test
+    void getUvidMatchWhenErrorRetrieving() throws IOException {
+        final var exception = new ApiErrorResponseException(
+            new HttpResponseException.Builder(HttpStatusCodes.STATUS_CODE_FORBIDDEN, "test case",
+                new HttpHeaders()));
+        when(apiClientService.getApiClient(PASSTHROUGH_HEADER)).thenThrow(exception);
+
+        final var thrown = assertThrows(PscLookupServiceException.class,
+            () -> testService.getUvidMatchFromPscData(transaction, PSC_VERIFICATION_DATA,
+                INDIVIDUAL, PASSTHROUGH_HEADER));
+
+        assertThat(thrown.getMessage(),
+            is("Error Retrieving PSC details for " + PSC_ID + ": 403 test case"));
+    }
+
+    @Disabled
+    @Test
+    void getUvidMatchFromPscDataWhenTypeNotRecognised() {
+        //TODO - Add if it is agreed to implement RO verifications
+    }
+}
