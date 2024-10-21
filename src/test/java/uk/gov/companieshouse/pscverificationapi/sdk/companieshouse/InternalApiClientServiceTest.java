6eff60b84bf90eb792dd3780b49c1cc1384a8f5d
chrislewis <clewis1@companieshouse.gov.uk>
2024-10-21 16:09:33 +0100

    Removing unnecessary environmentReader call.

--- src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/InternalApiClientServiceTest.java
+++ src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/InternalApiClientServiceTest.java
@@ -9,46 +9,36 @@ import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.InternalApiClient;
-import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.sdk.manager.ApiClientManager;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class InternalApiClientServiceTest {
-
-    private static final String CONFIGURED_INTERNAL_API_URL = "http://api.chs.local:4001";
 
     private InternalApiClientService service;
 
     @Mock
     private InternalApiClient internalApiClient;
 
-    @Mock
-    private EnvironmentReader environmentReader;
-
     @BeforeEach
     void setUp() {
-        service = new InternalApiClientService(environmentReader);
+        service = new InternalApiClientService();
     }
 
     @Test
-    @DisplayName("getInternalApiClient returns internal API client with an overridden internal API URL value")
-    void getInternalApiClientOverridesInternalApiUrlUsed() {
+    @DisplayName("getInternalApiClient")
+    void getInternalApiClient() {
 
         try (final MockedStatic<ApiClientManager> apiClientManager = Mockito.mockStatic(ApiClientManager.class)) {
             // Given
             apiClientManager.when(ApiClientManager::getPrivateSDK).thenReturn(internalApiClient);
-            when(environmentReader.getMandatoryString("INTERNAL_API_URL")).thenReturn(CONFIGURED_INTERNAL_API_URL);
 
             // When
             final InternalApiClient internalApiClientReturned = service.getInternalApiClient();
 
             // Then
-            verify(internalApiClient).setInternalBasePath(CONFIGURED_INTERNAL_API_URL);
             assertThat(internalApiClientReturned, is(internalApiClient));
         }
     }
