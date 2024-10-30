21f97906bbdecd61aa6c8614a8a8760429a1ad81
cryder-ch <cryder@companieshouse.gov.uk>
2024-10-30 15:26:16 +0000

    "formatted terraform files"

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -30,25 +30,25 @@ module "ecs-service" {
   task_execution_role_arn = data.aws_iam_role.ecs_cluster_iam_role.arn
 
   # Load balancer configuration
-  lb_listener_arn                 = data.aws_lb_listener.service_lb_listener.arn
-  lb_listener_rule_priority       = local.lb_listener_rule_priority
-  lb_listener_paths               = local.lb_listener_paths
-  multilb_setup                   = true
-  multilb_listeners               = {
-    "priv-api-lb": {
-          listener_arn            = data.aws_lb_listener.secondary_lb_listener.arn,
-          load_balancer_arn       = data.aws_lb.secondary_lb.arn
-      }
-    "pub-api-lb": {
-      load_balancer_arn           = data.aws_lb.service_lb.arn
-      listener_arn                = data.aws_lb_listener.service_lb_listener.arn
+  lb_listener_arn           = data.aws_lb_listener.service_lb_listener.arn
+  lb_listener_rule_priority = local.lb_listener_rule_priority
+  lb_listener_paths         = local.lb_listener_paths
+  multilb_setup             = true
+  multilb_listeners = {
+    "priv-api-lb" : {
+      listener_arn      = data.aws_lb_listener.secondary_lb_listener.arn,
+      load_balancer_arn = data.aws_lb.secondary_lb.arn
+    }
+    "pub-api-lb" : {
+      load_balancer_arn = data.aws_lb.service_lb.arn
+      listener_arn      = data.aws_lb_listener.service_lb_listener.arn
     }
   }
 
   # ECS Task container health check
   use_task_container_healthcheck = true
-  healthcheck_path          = local.healthcheck_path
-  healthcheck_matcher       = local.healthcheck_matcher
+  healthcheck_path               = local.healthcheck_path
+  healthcheck_matcher            = local.healthcheck_matcher
 
   # Docker container details
   docker_registry   = var.docker_registry
@@ -74,14 +74,14 @@ module "ecs-service" {
   fargate_subnets                    = local.application_subnet_ids
 
   # Cloudwatch
-  cloudwatch_alarms_enabled = var.cloudwatch_alarms_enabled
+  cloudwatch_alarms_enabled         = var.cloudwatch_alarms_enabled
   multilb_cloudwatch_alarms_enabled = var.multilb_cloudwatch_alarms_enabled
 
   # Service environment variable and secret configs
-  task_environment            = local.task_environment
-  task_secrets                = local.task_secrets
-  app_environment_filename    = local.app_environment_filename
-  use_set_environment_files   = local.use_set_environment_files
+  task_environment          = local.task_environment
+  task_secrets              = local.task_secrets
+  app_environment_filename  = local.app_environment_filename
+  use_set_environment_files = local.use_set_environment_files
 
 
   # eric options for eric running API module
--- terraform/groups/ecs-service/variables.tf
+++ terraform/groups/ecs-service/variables.tf
@@ -28,30 +28,30 @@ variable "docker_registry" {
 # Service performance and scaling configs
 # ------------------------------------------------------------------------------
 variable "desired_task_count" {
-  type = number
+  type        = number
   description = "The desired ECS task count for this service"
-  default = 1 # defaulted low for dev environments, override for production
+  default     = 1 # defaulted low for dev environments, override for production
 }
 variable "required_cpus" {
-  type = number
+  type        = number
   description = "The required cpu resource for this service. 1024 here is 1 vCPU"
-  default = 768 # defaulted low for dev environments, override for production
+  default     = 768 # defaulted low for dev environments, override for production
 }
 variable "required_memory" {
-  type = number
+  type        = number
   description = "The required memory for this service"
-  default = 1536 # defaulted low for node service in dev environments, override for production
+  default     = 1536 # defaulted low for node service in dev environments, override for production
 }
 
 variable "eric_cpus" {
-  type = number
+  type        = number
   description = "The required cpu resource for eric. 1024 here is 1 vCPU"
-  default = 256
+  default     = 256
 }
 variable "eric_memory" {
-  type = number
+  type        = number
   description = "The required memory for eric"
-  default = 512
+  default     = 512
 }
 variable "min_task_count" {
   type        = number
@@ -91,7 +91,7 @@ variable "service_scaledown_schedule" {
   # Typically used to stop all tasks in a service to save resource costs overnight.
   # E.g. a value of '55 19 * * ? *' would be Mon-Sun 7:55pm.  An empty string indicates that no schedule should be created.
 
-  default     = ""
+  default = ""
 }
 variable "service_scaleup_schedule" {
   type        = string
@@ -99,7 +99,7 @@ variable "service_scaleup_schedule" {
   # Typically used to start all tasks in a service after it has been shutdown overnight.
   # E.g. a value of '5 6 * * ? *' would be Mon-Sun 6:05am.  An empty string indicates that no schedule should be created.
 
-  default     = ""
+  default = ""
 }
 
 # ----------------------------------------------------------------------
@@ -147,14 +147,14 @@ variable "eric_version" {
 }
 
 variable "include_api_filing_public_specs" {
-  type = string
+  type    = string
   default = "1"
 }
 variable "include_pending_public_specs" {
-  type = string
+  type    = string
   default = "0"
 }
 variable "include_private_specs" {
-  type = string
+  type    = string
   default = "0"
 }
