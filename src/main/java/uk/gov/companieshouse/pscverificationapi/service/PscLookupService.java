b74802dd4d15659ba2b1ab18b808c3c9f1f8a3fa
PhilipGlover <pglover@companieshouse.gov.uk>
2024-11-20 10:20:05 +0000

    add call to PscLookupService to getPscIndividualFullRecord

--- src/main/java/uk/gov/companieshouse/pscverificationapi/service/PscLookupService.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/service/PscLookupService.java
@@ -4,6 +4,7 @@ import uk.gov.companieshouse.api.identityverification.model.UvidMatch;
 import uk.gov.companieshouse.api.model.psc.PscApi;
 import uk.gov.companieshouse.api.model.pscverification.PscVerificationData;
 import uk.gov.companieshouse.api.model.transaction.Transaction;
+import uk.gov.companieshouse.api.psc.IndividualFullRecord;
 import uk.gov.companieshouse.pscverificationapi.enumerations.PscType;
 import uk.gov.companieshouse.pscverificationapi.exception.PscLookupServiceException;
 
@@ -24,6 +25,17 @@ public interface PscLookupService {
     PscApi getPsc(Transaction transaction, PscVerificationData data, PscType pscType, final String ericPassThroughHeader)
         throws PscLookupServiceException;
 
+    /**
+     * Retrieve a PSC Individual Full Record by PscVerificationData.
+     *
+     * @param transaction           the Transaction
+     * @param data                  the PSC verification data
+     * @param pscType               the PSC Type
+     * @return the PSC Full Record details, if found
+     * @throws PscLookupServiceException if the PSC was not found or an error occurred
+     */
+    IndividualFullRecord getPscIndividualFullRecord(Transaction transaction, PscVerificationData data, PscType pscType)
+            throws PscLookupServiceException;
 
     /**
      * Retrieve a UvidMatch with the PSC data.
