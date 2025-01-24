de2af3a893a573a91f4c4c675ca7921a47deeb00
chrislewis <clewis1@companieshouse.gov.uk>
2025-01-24 14:18:23 +0000

    Fixing api client service tests

--- src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/ApiClientServiceTest.java
+++ src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/ApiClientServiceTest.java
@@ -1,15 +1,24 @@
 package uk.gov.companieshouse.pscverificationapi.sdk.companieshouse;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.ArgumentMatchers.isNotNull;
-import static org.mockito.Mockito.when;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.*;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.MockedConstruction;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.ApiClient;
+import uk.gov.companieshouse.environment.EnvironmentReader;
+import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
+import uk.gov.companieshouse.sdk.manager.ApiClientManager;
 
+@ExtendWith(MockitoExtension.class)
 class ApiClientServiceTest {
 
     @Mock
@@ -22,11 +31,25 @@ class ApiClientServiceTest {
         apiClientService = new ApiClientService();
     }
 
-    @Disabled("We can't get coverage here because of environment variables")
     @Test
     void getSdk() {
-        when(apiClientService.getApiClient("key")).thenReturn(apiClient);
-        assertThat(apiClient, isNotNull());
+        try (final MockedStatic<ApiClientManager> apiClientManager = Mockito.mockStatic(ApiClientManager.class)) {
+            apiClientManager.when(ApiClientManager::getSDK).thenReturn(apiClient);
+            try (MockedConstruction<EnvironmentReaderImpl> mocked = mockConstruction(EnvironmentReaderImpl.class)) {
+
+                // Given
+                final EnvironmentReader environmentReader = new EnvironmentReaderImpl();
+                when(environmentReader.getMandatoryString("API_URL")).thenReturn("API_URL");
+
+                // When
+                final ApiClient apiClientReturned = apiClientService.getApiClient("API_URL");
+
+                // Then
+                assertThat(apiClientReturned, is(apiClient));
+                assertThat(mocked.constructed(), hasSize(1));
+                assertThat(mocked.constructed().getFirst().getMandatoryString("API_URL"), is("API_URL") );
+            }
+        }
     }
 
 }
--- src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/InternalApiClientServiceTest.java
+++ src/test/java/uk/gov/companieshouse/pscverificationapi/sdk/companieshouse/InternalApiClientServiceTest.java
@@ -1,7 +1,6 @@
 package uk.gov.companieshouse.pscverificationapi.sdk.companieshouse;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,7 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.companieshouse.api.InternalApiClient;
 import uk.gov.companieshouse.environment.EnvironmentReader;
 import uk.gov.companieshouse.environment.impl.EnvironmentReaderImpl;
-import uk.gov.companieshouse.sdk.manager.ApiClientManager;
+import uk.gov.companieshouse.sdk.manager.ApiSdkManager;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -35,29 +34,27 @@ class InternalApiClientServiceTest {
         service = new InternalApiClientService("API_URL");
     }
 
-    @Disabled
     @Test
     @DisplayName("getInternalApiClient")
     void getInternalApiClient() {
 
-        try (final MockedStatic<ApiClientManager> apiClientManager = Mockito.mockStatic(ApiClientManager.class)) {
+        try (final MockedStatic<ApiSdkManager> apiSdkManager = Mockito.mockStatic(ApiSdkManager.class)) {
+            apiSdkManager.when(ApiSdkManager::getInternalSDK).thenReturn(internalApiClient);
             try (MockedConstruction<EnvironmentReaderImpl> mocked = mockConstruction(EnvironmentReaderImpl.class)) {
 
                 // Given
                 final EnvironmentReader environmentReader = new EnvironmentReaderImpl();
-                when(environmentReader.getMandatoryString("INTERNAL_API_URL")).thenReturn("API_URL");
-
-                apiClientManager.when(ApiClientManager::getPrivateSDK).thenReturn(internalApiClient);
+                when(environmentReader.getMandatoryString("INTERNAL_API_URL")).thenReturn("INTERNAL_API_URL");
 
                 // When
                 final InternalApiClient internalApiClientReturned = service.getInternalApiClient();
 
                 // Then
+                apiSdkManager.verify(ApiSdkManager::getInternalSDK);
                 verify(internalApiClient).setInternalBasePath("API_URL");
                 assertThat(internalApiClientReturned, is(internalApiClient));
-                assertThat(internalApiClientReturned.getInternalBasePath(), is("API_URL"));
                 assertThat(mocked.constructed(), hasSize(1));
-                assertThat(mocked.constructed().getFirst().getMandatoryString("INTERNAL_API_URL"), is("API_URL") );
+                assertThat(mocked.constructed().getFirst().getMandatoryString("INTERNAL_API_URL"), is("INTERNAL_API_URL") );
             }
         }
     }
