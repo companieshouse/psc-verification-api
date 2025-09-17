15e89100d549a0fee4463de373be65273433b33d
kieo-ch <koleary1@companieshouse.gov.uk>
2025-09-17 08:28:28 +0100

    Add Javadoc for config classes

--- src/main/java/uk/gov/companieshouse/pscverificationapi/config/ValidatorConfig.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/config/ValidatorConfig.java
@@ -5,9 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import uk.gov.companieshouse.pscverificationapi.enumerations.PscType;
 import uk.gov.companieshouse.pscverificationapi.validator.*;
 
+/**
+ * Configuration class for setting up the validation chain.
+ */
 @Configuration
 public class ValidatorConfig {
 
+    /**
+     * Creates and links the validation chain.
+     *
+     * @return the enabled validation chain
+     */
     @Bean
     public ValidationChainEnable verificationValidationEnable(final PscIdProvidedValidator pscIdProvidedValidator,
             final PscExistsValidator pscExistsValidator, final PscIsActiveValidator pscIsActiveValidator,
@@ -23,6 +31,11 @@ public class ValidatorConfig {
         return new VerificationValidationChain(PscType.INDIVIDUAL, pscIdProvidedValidator);
     }
 
+    /**
+     * Links the provided validators in sequence to form a validation chain.
+     *
+     * @param validators the validators to link
+     */
     private static void createValidationChain(BaseVerificationValidator... validators) {
         // Link all validators: a->b, b->c, c->d, etc
         for (int i = 0; i < validators.length - 1; i++) {
