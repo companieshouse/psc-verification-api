1b298acba2a26ab194f9bcd504e9531f299aea56
kieo-ch <koleary1@companieshouse.gov.uk>
2025-09-17 08:29:53 +0100

    Add Javadoc for exceptions

--- src/main/java/uk/gov/companieshouse/pscverificationapi/exception/NotImplementedException.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/exception/NotImplementedException.java
@@ -3,6 +3,9 @@ package uk.gov.companieshouse.pscverificationapi.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Exception for returning a 501 Not Implemented response.
+ */
 @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
 public class NotImplementedException extends RuntimeException {
 }
