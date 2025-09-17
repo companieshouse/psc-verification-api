dbc469bc58dfcab244f839ddde0532fe678c814f
kieo-ch <koleary1@companieshouse.gov.uk>
2025-09-17 08:31:22 +0100

    Add Javadoc for ErrorMapper

--- src/main/java/uk/gov/companieshouse/pscverificationapi/mapper/ErrorMapper.java
+++ src/main/java/uk/gov/companieshouse/pscverificationapi/mapper/ErrorMapper.java
@@ -9,6 +9,12 @@ import uk.gov.companieshouse.api.model.validationstatus.ValidationStatusError;
 import uk.gov.companieshouse.pscverificationapi.error.ErrorType;
 import uk.gov.companieshouse.pscverificationapi.error.LocationType;
 
+/**
+ * Mapper for converting {@link FieldError} objects to {@link ValidationStatusError}.
+ * <p>
+ * Uses MapStruct to map validation errors.
+ * </p>
+ */
 @Mapper(componentModel = "spring", imports = {ErrorType.class, LocationType.class})
 public interface ErrorMapper {
     @Mapping(target = "error", source = "defaultMessage")
